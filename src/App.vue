ab8897e6fcdebdbc1af045bcfa05e6abe1bb91e6
zhoucheng <zhoucheng133@outlook.com>
2026-01-06 20:48:21 +0800

    添加视频预览

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <InitView v-if="store().filePath.length==0" />
+  <ConfigView v-else />
 </template>
 
 <script setup lang="ts">
 import { onMounted } from 'vue';
 import { envCheck } from './utils/env_check';
 import InitView from './views/InitView.vue';
+import ConfigView from './views/ConfigView.vue';
 import store from './store';
 
 onMounted(()=>{
---
+++ src/views/ConfigView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="page">
+    <video :src="convertFileSrc(store().filePath)" width="400px" controls></video>
+  </div>
+</template>
+
+
+<script lang="ts" setup>
+import { convertFileSrc } from '@tauri-apps/api/core';
+import store from '../store';
+</script>
+
+<style scoped>
+
+</style>
