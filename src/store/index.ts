a68dd864267f0c6c630783f040543ff3f8f7df68
zhoucheng <zhoucheng133@outlook.com>
2026-01-09 23:01:43 +0800

    完成命令运行

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,3 +1,5 @@
+import { path } from "@tauri-apps/api";
+import { Child, Command } from "@tauri-apps/plugin-shell";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 
@@ -26,8 +28,59 @@ export default defineStore("index", ()=>{
   let outputPath = ref("");
   let running = ref(false);
 
-  function handler(){
+  let logs=ref<string[]>([]);
+
+  let child: Child | null = null;
+
+  async function handler(){
     running.value = !running.value;
+    logs.value = [];
+
+    if(running.value){
+      const fullOutputPath=await path.join(outputPath.value, "output.mp4");
+      let args=[
+        filePath.value,
+        '--output',
+        fullOutputPath,
+        '--thresh',
+        defaceConfig.value.thresh.toString(),
+        '--replacewith',
+        ReplaceWith[defaceConfig.value.replaceWith],
+        '--mask-scale',
+        defaceConfig.value.maskScale.toString(),
+      ];
+
+      if(defaceConfig.value.keepAudio){
+        args.push('-k');
+      }
+
+      console.log(args);
+
+      const command = Command.create(defaceConfig.value.keepAudio ? "defaceWithAudio" : "defaceWithoutAudio", args);
+
+      command.stdout.on('data', (line) => {
+        logs.value.unshift(line);
+        if(logs.value.length > 50){
+          logs.value.pop();
+        }
+      });
+
+      command.stderr.on('data', (line) => {
+        logs.value.unshift(line);
+        if(logs.value.length > 50){
+          logs.value.pop();
+        }
+      });
+
+      command.on('close', async (_) => {
+        running.value = false;
+      })
+
+      child = await command.spawn();
+    }else{
+      await child?.kill();
+      child = null;
+    }
   }
 
   return {
@@ -37,5 +90,6 @@ export default defineStore("index", ()=>{
     outputPath,
     running,
     handler,
+    logs
   };
 })
