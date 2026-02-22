b19b6b1c441c58aa3b4533d44ab523eadd8bff4b
zhoucheng <zhoucheng133@outlook.com>
2026-02-22 23:35:32 +0800

    精简一些代码

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -88,10 +88,10 @@ async fn stop_task(state: State<'_, CommandState>) -> Result<String, String> {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .setup(|app| {
+        .setup(|_app| {
             #[cfg(target_os = "macos")]
             {
-                let handle = app.handle();
+                let handle = _app.handle();
                 let locale = "zh";
                 let (app_menu_name, quit_label, edit_label, copy_label, paste_label, select_all_label, undo_label, redo_label, window_label, minimize_label, fullscreen_label, about_label, hide_label) = 
                 if locale == "zh" {
@@ -137,10 +137,8 @@ pub fn run() {
                 )?;
 
                 let menu = Menu::with_items(handle, &[&app_menu, &edit_menu, &window_menu])?;
-                app.set_menu(menu)?;
+                _app.set_menu(menu)?;
             }
-            #[cfg(target_os = "windows")]
-            app.hide_menu()?;
             Ok(())
         })
         .manage(CommandState(Arc::new(Mutex::new(None))))
